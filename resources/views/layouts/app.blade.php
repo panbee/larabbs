187bb659c8a39ecf08d301c306cfb1ddb190b04f
panda <panbee@163.com>
2018-07-04 03:29:25 +0000

    topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <!-- CSRF token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
+    <meta name="description" content="@yield('description','LaraBBS爱好者社区')">
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('styles')
