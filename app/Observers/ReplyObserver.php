5e1fb46a1b49337223ab7cc1672fd2c8daa4225a
panda <panbee@163.com>
2018-07-04 09:45:22 +0000

    notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,11 +12,15 @@ class ReplyObserver
 {
     public function creating(Reply $reply)
     {
-        $reply->content = clean($reply->content,'user_topic_body')
+        $reply->content = clean($reply->content,'user_topic_body');
     }
 
     public function created(Reply $reply)
     {
-        $reply->topic->increment('reply_count',1);
+        $topic = $reply->topic;
+        $topic->increment('reply_count',1);
+
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
     }
 }
