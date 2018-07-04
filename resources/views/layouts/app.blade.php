af32e6e42e17eff77de1548a1c5d9dfa668a1f0b
panda <panbee@163.com>
2018-07-04 02:44:43 +0000

    add editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <title>@yield('title','LaraBBS') - Laravel 进阶教程</title>
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
 
@@ -29,6 +30,6 @@
     </div>
 
     <script src="{{ asset('js/app.js') }}"></script>
-
+    @yield('scripts')
 </body>
 </html>
